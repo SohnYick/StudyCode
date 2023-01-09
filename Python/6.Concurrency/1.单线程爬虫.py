81cb431b631398b24516e1fd86481d453e385662
SohnYick <1404305564@qq.com>
2023-01-09 19:43:16 +0800

    save code

--- Python/6.Concurrency/1.单线程爬虫.py
+++ Python/6.Concurrency/1.单线程爬虫.py
@@ -1,37 +1,8 @@
-import requests
 import time
 
-class BookSpider:
-    """ 本类爬取豆瓣读书上面的数据 """
+import requests
 
-    def __init__(self,tag="编程",page_number=5):
-        """
-        根据给定的标签和要爬取的页数，构造一个对象。
-        :param tag: 标签
-        :param tag: 页数
-        """
-        self.urls = [
-            f"https://book.douban.com/tag/{tag}?start={(page-1)*20}&type=t"
-            for page in range(1,page_number+1)
-        ]
-
-        self.headers = {
-            "User-Agent" : "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Mobile Safari/537.36"
-        }
-
-    def craw(self):
-        """
-        抓取数据
-        :return: 返回字典数组，数组中每个字典包含网页相关的信息
-        """
-        pages = [] 
-        for url in self.urls:
-            pages.append({
-                "url":url,
-                "html":requests.get(url=url,headers=self.headers).text
-            })
-        return pages
-            
+from spider import BookSpider
 
 if __name__ == "__main__":
 
