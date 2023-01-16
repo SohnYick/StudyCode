ead462cec5373a8e8c1f54d20a901cf47fc6659f
SohnYick <1404305564@qq.com>
2023-01-17 07:02:40 +0800

    移除与本案例无关的所有内容。

--- Python/5.IO&Exception/1.try-except处理异常.py
+++
@@ -1,9 +0,0 @@
-
-if __name__ == "__main__":
-
-	try:
-		# 触发异常
-		5/0
-	except ZeroDivisionError:
-		# 处理异常
-		print("除数不能为零")
--- Python/5.IO&Exception/3.读取文件.py
+++
@@ -1,4 +0,0 @@
-with open("digits.txt") as file:
-	contents = file.read()
-
-print(f"文件内容为：\n{contents}")
